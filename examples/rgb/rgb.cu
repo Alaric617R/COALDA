c052fefd902162513e0d723e146d6a68bc3bb8ba
Ziang Li <ziangli@umich.edu>
2023-10-24 16:05:43 -0400

    Add comment

--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -4,12 +4,14 @@ struct pixel {
   int b;
 };
 
+// Commonly used, intuitive but uncoalesced
 __global__ void rgb_copy_interleaved(pixel *pixel_dst, pixel *pixel_src) {
   pixel_dst[threadIdx.x].r = pixel_src[threadIdx.x].r;
   pixel_dst[threadIdx.x].g = pixel_src[threadIdx.x].g;
   pixel_dst[threadIdx.x].b = pixel_src[threadIdx.x].b;
 };
 
+// Desired, coalesced
 __global__ void rgb_copy_coalesced(pixel *pixel_dst, pixel *pixel_src) {
   int *ptr_src = (int *)pixel_src;
   int *ptr_dst = (int *)pixel_dst;
