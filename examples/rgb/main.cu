482689ab08230c9f32bd4696cd69b8828190a11d
Ziang Li <ziangli@umich.edu>
2023-10-24 15:33:56 -0400

    Update rgb for testing copy

--- examples/rgb/main.cu
+++ examples/rgb/main.cu
@@ -1,26 +1,28 @@
 #include "rgb.cu"
 
-int main()
-{
-    pixel host_pixel[32];
-    for (int i = 0; i < 32; i++)
-    {
-        host_pixel[i] = {i, i, i};
-    }
-    pixel *device_pixel_src;
+int main() {
+  pixel host_pixel[32];
+  for (int i = 0; i < 32; i++) {
+    host_pixel[i] = {i, i, i};
+  }
+  pixel *device_pixel_src;
+  pixel *device_pixel_cpy;
 
-    cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
+  cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
+  cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
 
-    cudaMemcpy(device_pixel_src, host_pixel, 32 * sizeof(pixel), cudaMemcpyHostToDevice);
+  cudaMemcpy(device_pixel_src, host_pixel, 32 * sizeof(pixel),
+             cudaMemcpyHostToDevice);
 
-    dim3 dimGrid = (32, 1, 1);
-    dim3 dimBlock = (32, 1, 1);
+  dim3 dimGrid = (32, 1, 1);
+  dim3 dimBlock = (32, 1, 1);
 
-    rgb_smem_interleaved<<<dimGrid, dimBlock, 32 * sizeof(pixel)>>>(device_pixel_src);
-    cudaDeviceSynchronize();
+  rgb_copy_interleaved<<<dimGrid, dimBlock, 32 * sizeof(pixel)>>>(
+      device_pixel_src, device_pixel_copy);
+  cudaDeviceSynchronize();
 
-    cudaFree(device_pixel_src);
-    free(host_pixel);
+  cudaFree(device_pixel_src);
+  free(host_pixel);
 
-    return 0;
+  return 0;
 }
--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -1,36 +1,19 @@
-struct pixel
-{
-    int r;
-    int g;
-    int b;
+struct pixel {
+  int r;
+  int g;
+  int b;
 };
 
-__global__ void rgb_smem_interleaved(pixel *pixel_src)
-{
-    extern __shared__ pixel pixel_dst[];
-    // For testing bandwidth, load 4 times repeatedly
-    # pragma unroll
-    for (size_t i = 0; i < 4; i++)
-    {
-        pixel_dst[threadIdx.x].r = pixel_src[threadIdx.x].r;
-        pixel_dst[threadIdx.x].g = pixel_src[threadIdx.x].g;
-        pixel_dst[threadIdx.x].b = pixel_src[threadIdx.x].b;
-        __syncthreads();
-    }
+__global__ void rgb_copy_interleaved(pixel *pixel_dst, pixel *pixel_src) {
+  pixel_dst[threadIdx.x].r = pixel_src[threadIdx.x].r;
+  pixel_dst[threadIdx.x].g = pixel_src[threadIdx.x].g;
+  pixel_dst[threadIdx.x].b = pixel_src[threadIdx.x].b;
 };
 
-__global__ void rgb_smem_coalesced(pixel *pixel_src)
-{
-    extern __shared__ pixel pixel_dst[];
-    int *ptr_src = (int *)pixel_src;
-    int *ptr_dst = (int *)pixel_dst;
-    // For testing bandwidth, load 4 times repeatedly
-    # pragma unroll
-    for (size_t i = 0; i < 4; i++)
-    {
-        ptr_src[threadIdx.x + 0 * blockDim.x] = ptr_dst[threadIdx.x + 0 * blockDim.x];
-        ptr_src[threadIdx.x + 1 * blockDim.x] = ptr_dst[threadIdx.x + 1 * blockDim.x];
-        ptr_src[threadIdx.x + 2 * blockDim.x] = ptr_dst[threadIdx.x + 2 * blockDim.x];
-        __syncthreads();
-    }
+__global__ void rgb_copy_interleaved(pixel *pixel_dst, pixel *pixel_src) {
+  int *ptr_src = (int *)pixel_src;
+  int *ptr_dst = (int *)pixel_dst;
+  ptr_src[threadIdx.x + 0 * blockDim.x] = ptr_dst[threadIdx.x + 0 * blockDim.x];
+  ptr_src[threadIdx.x + 1 * blockDim.x] = ptr_dst[threadIdx.x + 1 * blockDim.x];
+  ptr_src[threadIdx.x + 2 * blockDim.x] = ptr_dst[threadIdx.x + 2 * blockDim.x];
 };
