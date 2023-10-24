dd7e95d149ae7b5ea202af18c8258eed59c67c32
Ziang Li <ziangli@umich.edu>
2023-10-24 16:03:15 -0400

    Add random data generation and print

--- examples/rgb/main.cu
+++ examples/rgb/main.cu
@@ -1,12 +1,23 @@
 #include "rgb.cu"
+#include <cstdio>
 
-int main() {
+int main()
+{
   pixel host_pixel[32];
-  for (int i = 0; i < 32; i++) {
-    host_pixel[i] = {i, i, i};
-  }
   pixel *device_pixel_src;
   pixel *device_pixel_cpy;
+
+  srand(1);
+  for (int i = 0; i < 32; i++)
+  {
+    host_pixel[i] = {rand() % 256, rand() % 256, rand() % 256};
+  }
+
+  printf("Host data initialized:\n");
+  for (int i = 0; i < 32; i++)
+  {
+    printf("%d %d %d %d\n", i, host_pixel[i].r, host_pixel[i].g, host_pixel[i].b);
+  }
 
   cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
   cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
@@ -18,7 +29,7 @@ int main() {
   dim3 dimBlock = (32, 1, 1);
 
   rgb_copy_interleaved<<<dimGrid, dimBlock, 32 * sizeof(pixel)>>>(
-      device_pixel_src, device_pixel_copy);
+      device_pixel_src, device_pixel_cpy);
   cudaDeviceSynchronize();
 
   cudaFree(device_pixel_src);
