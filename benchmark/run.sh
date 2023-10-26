9f01f0e017e0bf4c56326e477bafe828b1172f88
zihaoye <yezihhhao@gmail.com>
2023-10-26 18:57:55 -0400

    ptx insertion

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -9,22 +9,18 @@ rm -f default.profraw *_prof *_fplicm *.bc *.profdata *_output *.ll
 
 # ACTION NEEDED: If the gpu compute capability is different, please update it here
 # Convert source code to bitcode (IR).
-clang -emit-llvm -c --cuda-gpu-arch=sm_89 ${1}.cu -Xclang -disable-O0-optnone
-
-# Canonicalize natural loops (Ref: llvm.org/doxygen/LoopSimplify_8h_source.html)
-opt -passes='loop-simplify' ${1}.bc -o ${1}.ls.bc
-
-# Instrument profiler passes.
-opt -passes='pgo-instr-gen,instrprof' ${1}.ls.bc -o ${1}.ls.prof.bc
-
-# Note: We are using the New Pass Manager for these passes! 
+clang++ -c -emit-llvm --cuda-gpu-arch=sm_89 ${1}.cu
 
 # Generate binary executable with profiler embedded
-clang -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}_prof
+llc -march=nvptx64 -mcpu=sm_89 -filetype=asm ${1}-cuda-nvptx64-nvidia-cuda-sm_89.bc -o ${1}.ptx
+llc -filetype=obj ${1}.ll -o ${1}.o
+clang++ -L/opt/cuda/lib -lcudart ${1}.o ${1}.ptx -o ${1}_prof
+
+
 
 # When we run the profiler embedded executable, it generates a default.profraw file that contains the profile data.
-./${1}_prof > correct_output
+./${1}_prof > correct_outputs
 
 
 # Cleanup: Remove this if you want to retain the created files. And you do need to.
-rm -f default.profraw *_prof *_fplicm *.bc *.profdata *_output *.ll
+# rm -f default.profraw *_prof *_fplicm *.bc *.profdata *_output *.ll
