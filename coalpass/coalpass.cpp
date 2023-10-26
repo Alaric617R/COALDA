a16eda4c10ee3ad0c95e68d379f8f5ab75da03bd
zihaoye <yezihhhao@gmail.com>
2023-10-26 15:47:36 -0400

    starter pack include

---
+++ coalpass/coalpass.cpp
@@ -0,0 +1,41 @@
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include  <iostream>
+#include "llvm/Support/Format.h"
+#include "llvm/IR/CFG.h"
+
+using namespace llvm;
+
+namespace{
+struct CoalPass : public PassInfoMixin<CoalPass>{
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM){
+        // Add your code here
+
+
+        return PreservedAnalyses::all();
+    }
+};
+}
+
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "CoalPass", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+        ArrayRef<PassBuilder::PipelineElement>) {
+          if(Name == "coal"){
+            FPM.addPass(CoalPass());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
