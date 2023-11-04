892d14593ebfff90d06654ec4add33f025b9da05
Ziang Li <ziangli@umich.edu>
2023-11-03 21:16:24 -0400

    Change indexing style using tid

--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -17,19 +17,19 @@ struct pixel
 // Commonly used, intuitive but uncoalesced
 __global__ void rgb_copy_array_interleaved(int *pixel_dst, int *pixel_src)
 {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  pixel_dst[3 * idx + 0] = pixel_src[3 * idx + 0]; // r
-  pixel_dst[3 * idx + 1] = pixel_src[3 * idx + 1]; // g
-  pixel_dst[3 * idx + 2] = pixel_src[3 * idx + 2]; // b
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  pixel_dst[3 * tid + 0] = pixel_src[3 * tid + 0]; // r
+  pixel_dst[3 * tid + 1] = pixel_src[3 * tid + 1]; // g
+  pixel_dst[3 * tid + 2] = pixel_src[3 * tid + 2]; // b
 };
 
 // Desired, coalesced
 __global__ void rgb_copy_array_coalesced(int *pixel_dst, int *pixel_src)
 {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  pixel_dst[idx + 0 * blockDim.x] = pixel_src[idx + 0 * blockDim.x]; // r/g/b stride 1
-  pixel_dst[idx + 1 * blockDim.x] = pixel_src[idx + 1 * blockDim.x]; // r/g/b stride 2
-  pixel_dst[idx + 2 * blockDim.x] = pixel_src[idx + 2 * blockDim.x]; // r/g/b stride 3
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  pixel_dst[tid + 0 * blockDim.x] = pixel_src[tid + 0 * blockDim.x]; // r/g/b stride 1
+  pixel_dst[tid + 1 * blockDim.x] = pixel_src[tid + 1 * blockDim.x]; // r/g/b stride 2
+  pixel_dst[tid + 2 * blockDim.x] = pixel_src[tid + 2 * blockDim.x]; // r/g/b stride 3
 };
 
 // Commonly used, intuitive but uncoalesced
