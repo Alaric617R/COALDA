364e9dbf3eaed40f94613bd9732d697c13ab6419
Alaric <alaricyx@umich.edu>
2023-11-04 18:58:15 -0400

    added helper print functions

--- coalpass/coalpass.cpp
+++ coalpass/coalpass.cpp
@@ -1,13 +1,25 @@
 #include "coalpass.h"
 
 PreservedAnalyses coalPass::CoalPass::run(Function &F, FunctionAnalysisManager &FAM){
-        errs() << F.getName() << '\n';
         for (auto &bb : F){
             bb.printAsOperand(errs(), false); errs() << '\n';
-            for (auto &inst : bb){
-              errs() << inst << '\t' << inst.getOpcodeName() << '\n';
-            }
+            runOnOneBB(F, FAM);
         }
         errs() << "This is Alaric waving!\n";
         return PreservedAnalyses::all();
+}
+
+void coalPass::CoalPass::runOnOneBB(Function &F, FunctionAnalysisManager &FAM){
+
+}
+
+
+void coalPass::CoalPass::findAllContribution(Instruction* inst){
+    BasicBlock* parentBB = inst->getParent();
+    BasicBlock::reverse_iterator revStart;
+    for (BasicBlock::reverse_iterator curBack = parentBB->rbegin(); curBack != parentBB->rend(); curBack ++){
+        if (&(*curBack) == inst) {revStart = curBack; break;}
+    }
+    for (; revStart != parentBB->rend(); revStart ++)
+
 }
--- coalpass/coalpass.h
+++ coalpass/coalpass.h
@@ -9,6 +9,43 @@
 #include "llvm/Support/Format.h"
 #include "llvm/IR/CFG.h"
 
+#ifndef DEBUG
+#define DEBUG true
+#endif
+
+#define log_debug(expr, fmt, ...)  \
+                  if ((expr)) \
+                      errs() << llvm::format(fmt"\n", ## __VA_ARGS__ )
+
+#define log_debug_bb(expr, bbInfo, fmt, ...) \
+                  if ((expr)) {  \
+                    (bbInfo);    \
+                  errs() << '\t'; errs() << llvm::format(fmt"\n", ## __VA_ARGS__ );}
+
+#define seperator(num, charc) \
+                  for (int i = 0; i < (num); i++) errs() << charc;\
+                  errs() << '\n';
+
+#define sep_center(charc, center) \
+        for (int i = 0; i < 18; i ++) errs() << charc; \
+        errs() << '\t' << center << '\t'; \
+        for (int i = 0; i < 18; i ++) errs() << charc; \
+        errs() << '\n';
+
+template<typename ...Args>
+void tLog(Args && ...args)
+{
+    (errs() << ... << args);
+    errs() << '\n';
+}
+
+template<typename ...Args>
+void printBlockInfo(bool debug, BasicBlock* bb, Args && ...args){
+    if (!debug) return;
+    errs() << "Block: "; bb->printAsOperand(errs(), false); errs() << "\n";
+    tLog(args...);
+}
+ 
 using namespace llvm;
 
 
@@ -22,10 +59,15 @@ struct coalStoreInst{
  * Assumptions:
  * 1. All stores to aggregated data structure (Address calculated via getelememtptr) can be coalesced
  * 2. Data source for the above stores can also be coalesced when loads
+ * 3. Only do this in one basicblock
 */
 namespace coalPass{
 struct CoalPass : public PassInfoMixin<CoalPass>{
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+    void runOnOneBB(Function &F, FunctionAnalysisManager &FAM);
+
+    // only in parent BB of inst
+    void findAllContribution(Instruction* inst);
 
 
 
