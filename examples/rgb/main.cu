e72f8957a8e8fd270ea6c757e8dad4378617e230
ziangli <ziangli@umich.edu>
2023-11-06 15:02:31 -0500

    Remove struct implementation

--- examples/rgb/main.cu
+++ examples/rgb/main.cu
@@ -54,60 +54,6 @@ void test_rgb_array()
   cudaFree(device_pixel_cpy);
 }
 
-void test_rgb_struct()
-{
-  pixel host_pixel_src[32];
-  pixel host_pixel_res[32];
-  pixel *device_pixel_src;
-  pixel *device_pixel_cpy;
-
-  srand(1);
-  for (int i = 0; i < 32; i++)
-  {
-    host_pixel_src[i] = {rand() % 256, rand() % 256, rand() % 256};
-  }
-
-  printf("Host data initialized:\n");
-  for (int i = 0; i < 32; i++)
-  {
-    printf("%d %d %d %d\n", i, host_pixel_src[i].r, host_pixel_src[i].g, host_pixel_src[i].b);
-  }
-
-  cudaMalloc(&device_pixel_src, 32 * sizeof(pixel));
-  cudaMalloc(&device_pixel_cpy, 32 * sizeof(pixel));
-
-  cudaMemcpy(device_pixel_src, host_pixel_src, 32 * sizeof(pixel), cudaMemcpyHostToDevice);
-
-  dim3 dimGrid = dim3(1, 1, 1);
-  dim3 dimBlock = dim3(32, 1, 1);
-
-  rgb_copy_struct_interleaved<<<dimGrid, dimBlock>>>(device_pixel_cpy, device_pixel_src);
-  // rgb_copy_struct_whole<<<dimGrid, dimBlock>>>(device_pixel_cpy, device_pixel_src);
-  cudaDeviceSynchronize();
-
-  cudaMemcpy(host_pixel_res, device_pixel_cpy, 32 * sizeof(pixel), cudaMemcpyDeviceToHost);
-  cudaDeviceSynchronize();
-
-  bool success = true;
-  printf("Data after device internal copy:\n");
-  for (int i = 0; i < 32; i++)
-  {
-    printf("%d %d %d %d\n", i, host_pixel_res[i].r, host_pixel_res[i].g, host_pixel_res[i].b);
-    if (host_pixel_res[i] != host_pixel_src[i])
-    {
-      success = false;
-      printf("Error on pixel %d\n", i);
-    }
-  }
-  if (success)
-  {
-    printf("All matched!\n");
-  }
-
-  cudaFree(device_pixel_src);
-  cudaFree(device_pixel_cpy);
-}
-
 int main()
 {
   test_rgb_array();
--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -3,15 +3,6 @@ struct pixel
   int r;
   int g;
   int b;
-
-  bool operator==(const pixel &rhs)
-  {
-    return (r == rhs.r) && (g == rhs.g) && (b == rhs.b);
-  };
-  bool operator!=(const pixel &rhs)
-  {
-    return !(*this == rhs);
-  }
 };
 
 // Commonly used, intuitive but uncoalesced
@@ -34,26 +25,3 @@ __global__ void rgb_copy_array_coalesced(int *pixel_dst, int *pixel_src)
   pixel_dst[tid + 2 * blockDim.x] = pixel_src[tid + 2 * blockDim.x]; // r/g/b stride 3
 };
 
-// Commonly used, intuitive but uncoalesced
-__global__ void rgb_copy_struct_interleaved(pixel *pixel_dst, pixel *pixel_src)
-{
-  pixel_dst[threadIdx.x].r = pixel_src[threadIdx.x].r;
-  pixel_dst[threadIdx.x].g = pixel_src[threadIdx.x].g;
-  pixel_dst[threadIdx.x].b = pixel_src[threadIdx.x].b;
-};
-
-// Desired, coalesced
-__global__ void rgb_copy_struct_coalesced(pixel *pixel_dst, pixel *pixel_src)
-{
-  int *ptr_src = (int *)pixel_src;
-  int *ptr_dst = (int *)pixel_dst;
-  ptr_dst[threadIdx.x + 0 * blockDim.x] = ptr_src[threadIdx.x + 0 * blockDim.x];
-  ptr_dst[threadIdx.x + 1 * blockDim.x] = ptr_src[threadIdx.x + 1 * blockDim.x];
-  ptr_dst[threadIdx.x + 2 * blockDim.x] = ptr_src[threadIdx.x + 2 * blockDim.x];
-};
-
-__global__ void rgb_copy_struct_whole(pixel *pix_dst, pixel *pixel_src)
-{
-  int tid = blockIdx.x * blockDim.x + threadIdx.x;
-  pix_dst[tid] = pixel_src[tid];
-};
