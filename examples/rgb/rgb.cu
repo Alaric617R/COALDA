9f0d901ee6574888efc36d8d6a173a487c1a48ce
Ziang Li <ziangli@umich.edu>
2023-11-06 17:29:52 -0500

    Always assume valid tail element and remove logic

--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -23,7 +23,7 @@ __global__ void rgb_copy_array_coalesced(int *pixel_dst, int *pixel_src)
 
 // Commonly used, intuitive but uncoalesced
 // Functionality: averaging, new_r/g/b = 0.5 * curr_r/g/b + 0.5 * right_r/g/b
-// Assumption: the total number of pixels is a multiple of our TILE_WIDTH and aka blockDim.x
+// Assumption: the total number of pixels minus one is a multiple of our TILE_WIDTH and aka blockDim.x
 // Pattern: load data into shared memory, compute/read/write only on shared memory, write result in shared memory to global memory
 __global__ void rgb_smem_array_interleaved(int *pixel_dst, int *pixel_src, int size)
 {
@@ -35,16 +35,6 @@ __global__ void rgb_smem_array_interleaved(int *pixel_dst, int *pixel_src, int s
   int local_tid = threadIdx.x;
   __shared__ int pixel_smem_src[3 * TILE_WIDTH + 3];
   __shared__ int pixel_smem_dst[3 * TILE_WIDTH];
-  __shared__ bool valid_tail;
-
-  /*
-    Check if we want to load extra tail element
-    Note we only need one thread for computing this
-  */
-  if (local_tid == TILE_WIDTH - 1)
-  {
-    valid_tail = (global_tid + 1 < size); // e.g. global_tid = 31, size = 32, invalid tail; global_tid = 31, size = 33, valid tail
-  }
 
   /*
     Read input pixels to shared memory
@@ -53,7 +43,7 @@ __global__ void rgb_smem_array_interleaved(int *pixel_dst, int *pixel_src, int s
   pixel_smem_src[3 * local_tid + 0] = pixel_src[3 * global_tid + 0]; // r
   pixel_smem_src[3 * local_tid + 1] = pixel_src[3 * global_tid + 1]; // g
   pixel_smem_src[3 * local_tid + 2] = pixel_src[3 * global_tid + 2]; // b
-  if (valid_tail && local_tid < 3)
+  if (local_tid < 3)                                                 // In this assumption, we always want to load tail
   {
     pixel_smem_src[3 * TILE_WIDTH + local_tid] = pixel_src[3 * TILE_WIDTH + local_tid];
   }
