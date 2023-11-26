6bf6ce2b6e136b5fd62fd7f3f854d06e31f8b419
Ziang Li <ziangli@umich.edu>
2023-11-26 00:53:08 -0500

    Implement function for calling brightness kernel

--- examples/rgb/main.cu
+++ examples/rgb/main.cu
@@ -3,6 +3,8 @@
 #include "ppm_utils.h"
 #include <cstdio>
 
+#define TILE_WIDTH 32
+
 void test_rgb_array()
 {
   const dim3 dimGrid = dim3(1, 1, 1);
@@ -54,6 +56,26 @@ void test_rgb_array()
 
   cudaFree(device_pixel_src);
   cudaFree(device_pixel_cpy);
+}
+
+void test_increase_brightness()
+{
+  int width, height;
+  int *host_pixel_src = read_ppm("images/1.ppm", width, height);
+  const dim3 dimGrid = dim3(1, 1, 1);
+  const dim3 dimBlock = dim3(TILE_WIDTH, 1, 1);
+  int num_pixels = width * height;
+  int host_pixel_res[3 * num_pixels];
+  int *device_pixel_src;
+  int *device_pixel_cpy;
+  cudaMalloc(&device_pixel_src, 3 * num_pixels * sizeof(int));
+  cudaMalloc(&device_pixel_cpy, 3 * num_pixels * sizeof(int));
+  cudaMemcpy(device_pixel_src, host_pixel_src, 3 * num_pixels * sizeof(int), cudaMemcpyHostToDevice);
+  rgb_increase_brightness(device_pixel_cpy, device_pixel_src, width * height, 1.2);
+  cudaDeviceSynchronize();
+  cudaMemcpy(host_pixel_res, device_pixel_cpy, 3 * num_pixels * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  write_ppm("images/1_modified.ppm", host_pixel_res, width, height);
 }
 
 int main()
