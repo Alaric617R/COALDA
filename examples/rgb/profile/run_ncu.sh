0ec6f1cfad3e32ee44b2a951d89a89a47396a241
Ziang Li <ziangli@umich.edu>
2023-11-28 00:12:05 -0500

    Update ncu command

--- examples/rgb/profile/run_ncu.sh
+++ examples/rgb/profile/run_ncu.sh
@@ -1,1 +1,2 @@
-sudo ncu --kernel-name rgb_increase_brightness --launch-skip 0 --launch-count 1 -o ncu_report --sections MemoryWorkloadAnalysis_Chart "./a.out"
+sudo ncu --kernel-name rgb_increase_brightness --launch-skip 0 --launch-count 1 -o uncoalesced_chart -f --section MemoryWorkloadAnalysis_Chart "./a.out"
+sudo ncu --kernel-name rgb_increase_brightness_coalesced --launch-skip 0 --launch-count 1 -o coalesced_chart -f --section MemoryWorkloadAnalysis_Chart "./a.out"
