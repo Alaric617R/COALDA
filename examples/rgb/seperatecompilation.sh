a911f85cc64b7ec0a42fc2adb1022e3aae5c8132
hanlinbi <hanlinbi@umich.edu>
2023-11-28 12:16:57 -0500

    c

--- examples/rgb/seperatecompilation.sh
+++ examples/rgb/seperatecompilation.sh
@@ -2,23 +2,41 @@
 
 # Set paths to your CUDA and Clang installations if they're not in your PATH
 CUDA_PATH="/opt/cuda/bin"
-CLANG_PATH="/usr/bin/clang"
+CLANG_PATH="/usr/bin"
 
 # Set the target GPU architecture
 GPU_ARCH="sm_75"
 
+rm *.bc *.ll *.ptx
+
+#following the below process
+# "nvptx64-nvidia-cuda" - "clang", inputs: ["rgb.cu"], output: "/tmp/rgb-c42443/rgb-sm_35.s"
+# "nvptx64-nvidia-cuda" - "NVPTX::Assembler", inputs: ["/tmp/rgb-c42443/rgb-sm_35.s"], output: "/tmp/rgb-d56ddb/rgb-sm_35.o"
+# "nvptx64-nvidia-cuda" - "NVPTX::Linker", inputs: ["/tmp/rgb-d56ddb/rgb-sm_35.o", "/tmp/rgb-c42443/rgb-sm_35.s"], output: "/tmp/rgb-25be7e.fatbin"
+# "x86_64-pc-linux-gnu" - "clang", inputs: ["rgb.cu", "/tmp/rgb-25be7e.fatbin"], output: "/tmp/rgb-2642c4.o"
+# "nvptx64-nvidia-cuda" - "clang", inputs: ["main.cu"], output: "/tmp/main-99237c/main-sm_35.s"
+# "nvptx64-nvidia-cuda" - "NVPTX::Assembler", inputs: ["/tmp/main-99237c/main-sm_35.s"], output: "/tmp/main-148b92/main-sm_35.o"
+# "nvptx64-nvidia-cuda" - "NVPTX::Linker", inputs: ["/tmp/main-148b92/main-sm_35.o", "/tmp/main-99237c/main-sm_35.s"], output: "/tmp/main-93db48.fatbin"
+# "x86_64-pc-linux-gnu" - "clang", inputs: ["main.cu", "/tmp/main-93db48.fatbin"], output: "/tmp/main-9b99b7.o"
+# "x86_64-pc-linux-gnu" - "GNU::Linker", inputs: ["/tmp/rgb-2642c4.o", "/tmp/main-9b99b7.o"], output: "a.out"
+
 # Compile the CUDA device code for rgb.cu
-$CLANG_PATH/clang --cuda-gpu-arch=$GPU_ARCH -c rgb.cu -o /tmp/rgb-sm_75.s
+# $CLANG_PATH/clang --cuda-gpu-arch=$GPU_ARCH -c rgb.cu -o ./tmp/rgb-sm_75.s
+clang++ --cuda-gpu-arch=sm_75 --cuda-device-only -emit-llvm -c rgb.cu -o rgb.bc -Xclang -disable-O0-optnone
 
 # Assemble the device code
-$CUDA_PATH/ptxas /tmp/rgb-sm_75.s -o /tmp/rgb-sm_75.o
+$CUDA_PATH/ptxas -c optimized.ptx --gpu-name=sm_75
 
 # Link to create a fat binary
-$CUDA_PATH/fatbinary --create=/tmp/rgb-b8d9fd.fatbin --image=profile=compute_$GPU_ARCH,file=/tmp/rgb-sm_35.o
+$CUDA_PATH/fatbinary -create optimized.fatbin --image=profile=sm_75,file=elf.o --64
 
-# Compile the host code for rgb.cu
-$CLANG_PATH/clang -c rgb.cu -o /tmp/rgb-e979a2.o --cuda-path=$CUDA_PATH --cuda-include-path=$CUDA_PATH/include
+#TODO: Compile rgb.cu to host only
+
+
+
+# Compile the host code for main.cu
+clang++ --cuda-gpu-arch=sm_75 --cuda-host-only -emit-llvm -c main.cu -o main.bc -Xclang -disable-O0-optnone
 
 # Repeat the process for main.cu
-$CLANG_PATH/clang --cuda-gpu-arch=$GPU_ARCH -c main.cu -o /tmp/main-sm_35.s
-$CUDA_PATH/ptxas /tmp/main-sm_35.s -o /tmp/main-sm_35.o
+$CLANG_PATH/clang --cuda-gpu-arch=$GPU_ARCH -c main.cu -o ./tmp/main-sm_75.s
+$CUDA_PATH/ptxas ./tmp/main-sm_75.s -o ./tmp/main-sm_75.o
