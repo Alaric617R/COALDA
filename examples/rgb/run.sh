4525aaf0495dfa0f2261f63e7880b7ca81542d13
Ziang Li <ziangli@umich.edu>
2023-11-29 19:52:00 -0500

    Add compile command for cuda-11.7

--- examples/rgb/run.sh
+++ examples/rgb/run.sh
@@ -2,4 +2,8 @@
 
 rm ./a.out
 clang++  main.cu -o a.out --cuda-gpu-arch=sm_89 -L/opt/cuda/lib -lcudart_static -ldl -lrt -pthread
+
+# If using cuda-11.7
+clang++  main.cu -o a.out -stdlib=libc++ --cuda-path=/opt/cuda-11.7 --cuda-gpu-arch=sm_86 -L/opt/cuda-11.7/lib64 -lcudart_static -ldl -lrt -pthread
+
 ./a.out
