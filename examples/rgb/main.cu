0b988614e2c74fec6ed4da7be0c21f96afbc0303
ziangli <ziangli@umich.edu>
2023-12-03 17:32:22 -0500

    Implement unrolled brightness kernel

--- examples/rgb/main.cu
+++ examples/rgb/main.cu
@@ -84,8 +84,36 @@ void test_increase_brightness() {
   cudaFree(device_pixel_cpy);
 }
 
+void test_increase_brightness_unroll() {
+  int width, height;
+  int *host_pixel_src = read_ppm("images/1.ppm", width, height);
+  long long total_pixel = width * height / 4;
+  int num_block = (total_pixel + TILE_WIDTH - 1) / TILE_WIDTH;
+  const dim3 dimGrid = dim3(num_block, 1, 1);
+  const dim3 dimBlock = dim3(TILE_WIDTH, 1, 1);
+  int num_pixels = width * height;
+  int host_pixel_res[4 * 3 * num_pixels];
+  int *device_pixel_src;
+  int *device_pixel_cpy;
+  cudaMalloc(&device_pixel_src, 4 * 3 * num_pixels * sizeof(int));
+  cudaMalloc(&device_pixel_cpy, 4 * 3 * num_pixels * sizeof(int));
+  cudaMemcpy(device_pixel_src, host_pixel_src, 4 * 3 * num_pixels * sizeof(int),
+             cudaMemcpyHostToDevice);
+  rgb_increase_brightness_coalesced<<<dimGrid, dimBlock>>>(
+      device_pixel_cpy, device_pixel_src, width * height, 1.8);
+  cudaDeviceSynchronize();
+  cudaMemcpy(host_pixel_res, device_pixel_cpy, 4 * 3 * num_pixels * sizeof(int),
+             cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  write_ppm("images/1_modified.ppm", host_pixel_res, width, height);
+  delete[] host_pixel_src;
+  cudaFree(device_pixel_src);
+  cudaFree(device_pixel_cpy);
+}
+
 int main() {
   // test_rgb_array();
-  test_increase_brightness();
+  // test_increase_brightness();
+  test_increase_brightness_unroll();
   return 0;
 }
--- examples/rgb/rgb.cu
+++ examples/rgb/rgb.cu
@@ -156,3 +156,220 @@ __global__ void rgb_increase_brightness_coalesced(int *pixel_dst,
   pixel_dst[global_tid + 2 * blockDim.x] =
       pixel_smem_dst[local_tid + 2 * blockDim.x];  // b
 };
+
+__global__ void rgb_increase_brightness_unroll(int *pixel_dst, int *pixel_src,
+                                        int size, float factor) {
+  /*
+    Declaration
+  */
+  int global_tid = blockIdx.x * blockDim.x + threadIdx.x;
+  int local_tid = threadIdx.x;
+  __shared__ int pixel_smem_src[4 * 3 * TILE_WIDTH];
+  __shared__ int pixel_smem_dst[4 * 3 * TILE_WIDTH];
+
+  /*
+    Read input pixels to shared memory
+  */
+  pixel_smem_src[4 * 3 * local_tid + 0] = pixel_src[4 * 3 * global_tid + 0];  // r
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 1] = pixel_src[4 * 3 * global_tid + 1];  // g
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 2] = pixel_src[4 * 3 * global_tid + 2];  // b
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 3] = pixel_src[4 * 3 * global_tid + 3];  // r
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 4] = pixel_src[4 * 3 * global_tid + 4];  // g
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 5] = pixel_src[4 * 3 * global_tid + 5];  // b
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 6] = pixel_src[4 * 3 * global_tid + 6];  // r
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 7] = pixel_src[4 * 3 * global_tid + 7];  // g
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 8] = pixel_src[4 * 3 * global_tid + 8];  // b
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 9] = pixel_src[4 * 3 * global_tid + 9];  // r
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 10] = pixel_src[4 * 3 * global_tid + 10];  // g
+  __syncthreads();
+  pixel_smem_src[4 * 3 * local_tid + 11] = pixel_src[4 * 3 * global_tid + 11];  // b
+  __syncthreads();
+
+  /*
+    Computation
+  */
+  pixel_smem_dst[4 * 3 * local_tid + 0] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 0])));
+  pixel_smem_dst[4 * 3 * local_tid + 1] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 1])));
+  pixel_smem_dst[4 * 3 * local_tid + 2] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 2])));
+  pixel_smem_dst[4 * 3 * local_tid + 3] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 3])));
+  pixel_smem_dst[4 * 3 * local_tid + 4] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 4])));
+  pixel_smem_dst[4 * 3 * local_tid + 5] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 5])));
+  pixel_smem_dst[4 * 3 * local_tid + 6] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 6])));
+  pixel_smem_dst[4 * 3 * local_tid + 7] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 7])));
+  pixel_smem_dst[4 * 3 * local_tid + 8] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 8])));
+  pixel_smem_dst[4 * 3 * local_tid + 9] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 9])));
+  pixel_smem_dst[4 * 3 * local_tid + 10] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 10])));
+  pixel_smem_dst[4 * 3 * local_tid + 11] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 11])));
+  __syncthreads();
+
+  /*
+    Write result to destination
+  */
+  pixel_dst[4 * 3 * global_tid + 0] = pixel_smem_dst[4 * 3 * local_tid + 0];  // r
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 1] = pixel_smem_dst[4 * 3 * local_tid + 1];  // g
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 2] = pixel_smem_dst[4 * 3 * local_tid + 2];  // b
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 3] = pixel_smem_dst[4 * 3 * local_tid + 3];  // r
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 4] = pixel_smem_dst[4 * 3 * local_tid + 4];  // g
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 5] = pixel_smem_dst[4 * 3 * local_tid + 5];  // b
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 6] = pixel_smem_dst[4 * 3 * local_tid + 6];  // r
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 7] = pixel_smem_dst[4 * 3 * local_tid + 7];  // g
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 8] = pixel_smem_dst[4 * 3 * local_tid + 8];  // b
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 9] = pixel_smem_dst[4 * 3 * local_tid + 9];  // r
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 10] = pixel_smem_dst[4 * 3 * local_tid + 10];  // g
+  __syncthreads();
+  pixel_dst[4 * 3 * global_tid + 11] = pixel_smem_dst[4 * 3 * local_tid + 11];  // b
+  __syncthreads();
+};
+
+__global__ void rgb_increase_brightness_coalesced_unroll(int *pixel_dst,
+                                                  int *pixel_src, int size,
+                                                  float factor) {
+  /*
+    Declaration
+  */
+  int global_tid = 4 * 3 * blockIdx.x * blockDim.x + threadIdx.x;
+  int local_tid = threadIdx.x;
+  __shared__ int pixel_smem_src[4 * 3 * TILE_WIDTH];
+  __shared__ int pixel_smem_dst[4 * 3 * TILE_WIDTH];
+
+  /*
+    Read input pixels to shared memory
+  */
+  pixel_smem_src[local_tid + 0 * blockDim.x] =
+      pixel_src[global_tid + 0 * blockDim.x];  // r
+  __syncthreads();
+  pixel_smem_src[local_tid + 1 * blockDim.x] =
+      pixel_src[global_tid + 1 * blockDim.x];  // g
+  __syncthreads();
+  pixel_smem_src[local_tid + 2 * blockDim.x] =
+      pixel_src[global_tid + 2 * blockDim.x];  // b
+  __syncthreads();
+  pixel_smem_src[local_tid + 3 * blockDim.x] =
+      pixel_src[global_tid + 3 * blockDim.x];  // r
+  __syncthreads();
+  pixel_smem_src[local_tid + 4 * blockDim.x] =
+      pixel_src[global_tid + 4 * blockDim.x];  // g
+  __syncthreads();
+  pixel_smem_src[local_tid + 5 * blockDim.x] =
+      pixel_src[global_tid + 5 * blockDim.x];  // b
+  __syncthreads();
+  pixel_smem_src[local_tid + 6 * blockDim.x] =
+      pixel_src[global_tid + 6 * blockDim.x];  // r
+  __syncthreads();
+  pixel_smem_src[local_tid + 7 * blockDim.x] =
+      pixel_src[global_tid + 7 * blockDim.x];  // g
+  __syncthreads();
+  pixel_smem_src[local_tid + 8 * blockDim.x] =
+      pixel_src[global_tid + 8 * blockDim.x];  // b
+  __syncthreads();
+  pixel_smem_src[local_tid + 9 * blockDim.x] =
+      pixel_src[global_tid + 9 * blockDim.x];  // r
+  __syncthreads();
+  pixel_smem_src[local_tid + 10 * blockDim.x] =
+      pixel_src[global_tid + 10 * blockDim.x];  // g
+  __syncthreads();
+  pixel_smem_src[local_tid + 11 * blockDim.x] =
+      pixel_src[global_tid + 11 * blockDim.x];  // b
+  __syncthreads();
+
+  /*
+    Computation
+  */
+  pixel_smem_dst[4 * 3 * local_tid + 0] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 0])));
+  pixel_smem_dst[4 * 3 * local_tid + 1] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 1])));
+  pixel_smem_dst[4 * 3 * local_tid + 2] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 2])));
+  pixel_smem_dst[4 * 3 * local_tid + 3] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 3])));
+  pixel_smem_dst[4 * 3 * local_tid + 4] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 4])));
+  pixel_smem_dst[4 * 3 * local_tid + 5] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 5])));
+  pixel_smem_dst[4 * 3 * local_tid + 6] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 6])));
+  pixel_smem_dst[4 * 3 * local_tid + 7] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 7])));
+  pixel_smem_dst[4 * 3 * local_tid + 8] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 8])));
+  pixel_smem_dst[4 * 3 * local_tid + 9] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 9])));
+  pixel_smem_dst[4 * 3 * local_tid + 10] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 10])));
+  pixel_smem_dst[4 * 3 * local_tid + 11] =
+      min(255, (int)(factor * (pixel_smem_src[4 * 3 * local_tid + 11])));
+  __syncthreads();
+
+  /*
+    Write result to destination
+  */
+  pixel_dst[global_tid + 0 * blockDim.x] =
+      pixel_smem_dst[local_tid + 0 * blockDim.x];  // r
+  __syncthreads();
+  pixel_dst[global_tid + 1 * blockDim.x] =
+      pixel_smem_dst[local_tid + 1 * blockDim.x];  // g
+  __syncthreads();
+  pixel_dst[global_tid + 2 * blockDim.x] =
+      pixel_smem_dst[local_tid + 2 * blockDim.x];  // b
+  __syncthreads();
+  pixel_dst[global_tid + 3 * blockDim.x] =
+      pixel_smem_dst[local_tid + 3 * blockDim.x];  // r
+  __syncthreads();
+  pixel_dst[global_tid + 4 * blockDim.x] =
+      pixel_smem_dst[local_tid + 4 * blockDim.x];  // g
+  __syncthreads();
+  pixel_dst[global_tid + 5 * blockDim.x] =
+      pixel_smem_dst[local_tid + 5 * blockDim.x];  // b
+  __syncthreads();
+  pixel_dst[global_tid + 6 * blockDim.x] =
+      pixel_smem_dst[local_tid + 6 * blockDim.x];  // r
+  __syncthreads();
+  pixel_dst[global_tid + 7 * blockDim.x] =
+      pixel_smem_dst[local_tid + 7 * blockDim.x];  // g
+  __syncthreads();
+  pixel_dst[global_tid + 8 * blockDim.x] =
+      pixel_smem_dst[local_tid + 8 * blockDim.x];  // b
+  __syncthreads();
+  pixel_dst[global_tid + 9 * blockDim.x] =
+      pixel_smem_dst[local_tid + 9 * blockDim.x];  // r
+  __syncthreads();
+  pixel_dst[global_tid + 10 * blockDim.x] =
+      pixel_smem_dst[local_tid + 10 * blockDim.x];  // g
+  __syncthreads();
+  pixel_dst[global_tid + 11 * blockDim.x] =
+      pixel_smem_dst[local_tid + 11 * blockDim.x];  // b
+  __syncthreads();
+};
