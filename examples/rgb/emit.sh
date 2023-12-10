d62738b3b6f561105d63e934a3d626fa4efb4b0f
Ziang Li <ziangli@umich.edu>
2023-12-10 01:51:48 -0500

    Emit coalesced main and revert run-pass

--- examples/rgb/emit.sh
+++ examples/rgb/emit.sh
@@ -2,5 +2,7 @@
 
 rm *.bc *.ll
 clang++ -std=c++20 -stdlib=libc++ --cuda-path=/opt/cuda-11.7 --cuda-gpu-arch=sm_86 -emit-llvm -c main.cu -Xclang -disable-O0-optnone
+clang++ -std=c++20 -stdlib=libc++ --cuda-path=/opt/cuda-11.7 --cuda-gpu-arch=sm_86 -emit-llvm -c main_coalesced.cu -Xclang -disable-O0-optnone
+
 llvm-dis *.bc
 
--- examples/rgb/run-pass.sh
+++ examples/rgb/run-pass.sh
@@ -6,7 +6,6 @@
 PATH2LIB="../../build/coalpass/CoalPass.so" # Specify your build directory in the project
 PASS=coal
 
-clang++ -std=c++20 -stdlib=libc++ --cuda-path=/opt/cuda-11.7 --cuda-gpu-arch=sm_86 -emit-llvm -c main.cu -Xclang -disable-O0-optnone
 opt -load-pass-plugin="${PATH2LIB}" -passes="${PASS}" main-cuda-nvptx64-nvidia-cuda-sm_86.bc -o main-cuda-nvptx64-nvidia-cuda-sm_86_opted.bc
 
 llvm-dis *.bc
